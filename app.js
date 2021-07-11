421eaffb68d310371f2085fcd4fa33c9fd9c0282
Irene_Liou <brtm55648@gmail.com>
2021-07-11 21:05:00 +0800

    feat: cpmplete CRUD function

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const restaurants = require('./restaurant.json')
 const mongoose = require('mongoose')
 const db = mongoose.connection
 const Restaurant = require('./models/restaurant.js')
+const restaurant = require('./models/restaurant.js')
 
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
@@ -62,14 +63,74 @@ app.post('/restaurants', (req, res) => {
     .catch(error => console.log(error))
 })
 
+//Edit restaurant
+app.get('/restaurants/:id/edit', (req, res) => {
+  const id = req.params.id
+
+  return Restaurant.findById(id)
+    .lean()
+    .then((restaurant) => res.render('edit', { restaurant }))
+    .catch(error => console.log(error))
+})
+
+app.post('/restaurants/:id/edit', (req, res) => {
+  const id = req.params.id
+  const name = req.body.name
+  const name_en = req.body.name_en
+  const category = req.body.category
+  const image = req.body.image
+  const location = req.body.location
+  const phone = req.body.phone
+  const google_map = req.body.google_map
+  const rating = req.body.rating
+  const description = req.body.description
+
+  return Restaurant.findById(id)
+    .thne((restaurnat) => {
+      restaurant.name = name
+      restaurant.name_en = name_en
+      restaurant.category = category
+      restaurant.image = image
+      restaurant.location = location
+      restaurant.phone = phone
+      restaurant.google_map = google_map
+      restaurant.rating = rating
+      restaurant.description = description
+      return restaurant.save()
+    })
+    .then(() => res.redirect(`/restaurants/${id}`))
+    .catch(error => console.log(error))
+})
+
+//Delete restaurant
+app.post('/restaurants/:id/delete', (req, res) => {
+  const id = req.params.id
+
+  return Restaurant.findById(id)
+    .then((restaurant) => {
+      return restaurant.remove()
+    })
+    .then(() => res.redirect('/'))
+    .catch(error => console.log(error))
+})
+
 
 app.get('/search', (req, res) => {
   const keyword = req.query.keyword.trim()
-  const filteredrestaurants = restaurants.results.filter(restaurant =>
-    restaurant.name.includes(keyword) || restaurant.category.includes(keyword)
-  )
 
-  res.render('index', { restaurants: filteredrestaurants, keyword: keyword })
+  return Restaurant.find()
+    .lean()
+    .then((restaurants) => {
+      restaurants = restaurants.filter(restaurant =>
+        restaurant.name.includes(keyword) || restaurant.category.includes(keyword)
+      )
+      if (restaurants.length === 0) {
+        res.render('index', { restaurants: restaurants, keyword: keyword, alert: `<h1 class="display-5 mt-5 text-info text-center">No results.</h1>` })
+      } else {
+        res.render('index', { restaurants: restaurants, keyword: keyword })
+      }
+    })
+    .catch(error => console.log(error))
 })
 
 app.listen(port, () => {
